e285dc2320f228b91b39ea4fcb062e7c03b87469
Guopeng Li <GLI21311@open-groupe.com>
2022-12-27 15:09:10 +0100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by LI Guopeng on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
